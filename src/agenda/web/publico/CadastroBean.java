6df6cdb0d0c802946ee2be14cc7567d551622476
Marcelo Ponciano <marceloponciano.eb@gmail.com>
2016-11-23 23:55:08 -0300

    Aula 6

--- src/agenda/web/publico/CadastroBean.java
+++ src/agenda/web/publico/CadastroBean.java
@@ -3,6 +3,9 @@ package agenda.web.publico;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import agenda.modelo.contato.Contato;
 import agenda.modelo.contato.ContatoRN;
 
@@ -10,14 +13,27 @@ import agenda.modelo.contato.ContatoRN;
 @RequestScoped
 public class CadastroBean {
 
+	static Logger logger = LoggerFactory.getLogger(CadastroBean.class);
+
 	private Contato contato = new Contato();
 
 	public String salvar() {
 
-		ContatoRN contatoRN = new ContatoRN();
-		
-	    contatoRN.salvar(contato);
-		
+		try {
+			
+			ContatoRN contatoRN = new ContatoRN();
+
+			contatoRN.salvar(contato);
+
+			logger.info("Contato salvo com sucesso!");
+
+		} catch (Exception e) {
+			
+			logger.error("Erro ao salvar o contato" , e.getMessage());
+			
+			e.printStackTrace();
+		}
+
 		contato = new Contato();
 
 		return "entrada?faces-redirect=true";
